a6e2d51029d51fcd8f94ab2f618b5376fd2fac4f
Danger <danger.recheng@hotmail.it>
2015-10-09 17:30:06 +0200

    version 1.0 stable maybe xD

--- fcp.py
+++ fcp.py
@@ -1,3 +1,20 @@
+'''
+    This file is part of Fantasy City Planner.
+    Fantasy City Planner is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    Fantasy City Planner is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Fantasy City Planner.  If not, see <http://www.gnu.org/licenses/>.
+    
+    @author DangerBlack
+    @version 1.0
+   
+'''
 import Image
 import ImageDraw
 import ImageFont
@@ -835,6 +852,7 @@ for infile in glob.glob("map/*.png"):
     if infile.endswith(str(mas)+".png"):
         mas=mas+1
 
+mas=mas+1
 filename='map/mappa_'+str(mas)
 img.save(filename+'.png')
 
